ce0baeaf0dfd37c51a4d80f4edcf3fef53270e59
Onesimus <ocpradhan@gmail.com>
2023-01-09 15:38:40 +0530

    Added a function which takes the number of squares/side to be created.

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const btn = document.querySelector(".btn");
 
 // Function for creating multiple divs
 const createDivs = (divs) => {
-  for (let i = 0; i < divs; i++) {
+  for (let i = 0; i < divs * divs; i++) {
     let divElement = document.createElement("div");
     divElement.className = "box";
     content.appendChild(divElement);
   }
+  content.style.gridTemplateColumns = `repeat(${divs}, 3rem)`;
+  content.style.gridTemplateRows = `repeat(${divs}, 3rem)`;
 };
 
-createDivs(256);
+createDivs(10);
