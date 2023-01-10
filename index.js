3d888851a0d5ba2a658b7bf70e20e603f3bd4ddf
Onesimus <ocpradhan@gmail.com>
2023-01-10 09:57:51 +0530

    Created a function for taking input from the user. Added a event listener to the button which first deletes the existing grid and then creates a new one according to the user input

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 "use: strict";
 
 // Targeting the elements
+const container = document.querySelector(".container");
 const content = document.querySelector(".content");
 const btn = document.querySelector(".btn");
 
-// Function for creating multiple divs
-const createDivs = (divs) => {
-  for (let i = 0; i < divs * divs; i++) {
+// Function for creating multiple divs according to the squares/side.
+const createDivs = (squaresOnSide) => {
+  for (let i = 0; i < squaresOnSide * squaresOnSide; i++) {
     let divElement = document.createElement("div");
     divElement.className = "box";
     content.appendChild(divElement);
   }
-  content.style.gridTemplateColumns = `repeat(${divs}, 3rem)`;
-  content.style.gridTemplateRows = `repeat(${divs}, 3rem)`;
+  content.style.gridTemplateColumns = `repeat(${squaresOnSide}, 3rem)`;
+  content.style.gridTemplateRows = `repeat(${squaresOnSide}, 3rem)`;
 };
 
-createDivs(10);
+// Function for taking input from the user
+const inputUser = () => {
+  promptUser = Number(prompt("Enter the number of squares/side"));
+
+  if (promptUser <= 100) {
+    createDivs(promptUser);
+  } else {
+    alert("Please enter a number less than or equal to 0");
+  }
+};
+
+// Event Listeners
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  // Removing the previous blocks and then creating a new divs
+  content.innerHTML = "";
+
+  // Taking inout from the user
+  inputUser();
+});
